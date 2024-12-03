000722361b7069e2cd7d4f603d835d58fccd511c
samippp <Sami.peng@hotmail.com>
2024-12-03 00:35:07 -0500

    finish

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="text-5xl">Hello! I am Sami.</div>
+        <div class="text-base">This is a very basic react app which is also coupled with tailwindcss and hosted on github.</div>
+
+        <div class="mt-[10%] w-[50%]">
+          To set up this project, I installed Node.js and npm and then ran npx create-react-app "name" and followed the insallation for tailwindcss.
+          This is very easy to do and there were very minimal difficulties as this is my prefered way to create web apps. Tailwind makes styling even easier.
+        </div>
       </header>
     </div>
   );
